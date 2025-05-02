f89f79d2a55698bd063e749b0edbb2268eaba585
Janudi <januditharushika@gmail.com>
2025-05-02 22:57:56 +0530

    feat: add subtraction, multiplication, and division functionality to calculator

--- calculator-api/util.js
+++ calculator-api/util.js
@@ -1,7 +1,12 @@
-const calculator ={
-    add: (val1, val2) => (val1 != undefined && val2 != undefined) ? val1 + val2 : 0,
-    subtract: (val1, val2) => (val1 != undefined && val2 != undefined) ? val1 - val2 : 0,
-    multiply: (val1, val2) => (val1 != undefined && val2 != undefined) ? val1 * val2 : 0,
-}
-
-export default calculator;
+const calculator = {
+    add: (val1, val2) => val1 + val2,
+    subtract: (val1, val2) => val1 - val2,
+    multiply: (val1, val2) => val1 * val2,
+    divide: (val1, val2) => {
+      if (val2 === 0) return "Cannot divide by zero";
+      return val1 / val2;
+    }
+  };
+  
+  export default calculator;
+  
